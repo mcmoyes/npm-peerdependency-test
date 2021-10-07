77b5f39e824b5ecd026c8aa9e39ed0d91f1c050e
darnedsock <info@darnedsock.com>
2021-10-07 00:03:51 -0400

    out to browser element instead of console

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import { Graphics } from 'pixi.js-legacy';
 
 const g = new Graphics();
 if (g.generateCanvasTexture) {
-    console.log(`using pixi.js-legacy`);
+     out("pixi.js-legacy is running smoothly")
 } else {
-    console.error(`a conflict is preventing pixi.js-legacy from accessing canvas-graphics features`);
+    out(`a conflict is preventing pixi.js-legacy from accessing canvas-graphics features`);
+}
+
+function out(text) {
+    document.getElementsByTagName('body')[0].textContent = text;
 }
